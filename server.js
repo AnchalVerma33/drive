3f3aa9b32df4a4de7f3bb61827d3cd4db1d975a9
LAPTOP-SLFMTQK4\abhis <thakurabhinav17122001@gmail.com>
2021-06-16 19:02:15 +0530

    :

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express=require('express')
 const morgan=require('morgan')
 const colors=require('colors')
+require('dotenv').config({path:'./config/dev.env'})
 const app=express()
 const PORT=process.env.PORT||5000
+
+if(process.env.MODE==='development')
+    app.use(morgan('dev'))
+
 app.listen(PORT,()=>{console.log(`Server running on the port ${PORT}`.yellow)})
