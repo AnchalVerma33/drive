32e3c70ad8b84720edd078e14f52bf56188677f5
LAPTOP-SLFMTQK4\abhis <thakurabhinav17122001@gmail.com>
2021-06-17 19:48:45 +0530

    Login register done

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
 const express=require('express')
 const morgan=require('morgan')
-const colors=require('colors')
+require('colors')
 require('dotenv').config({path:'./config/dev.env'})
+const connectDB =require('./src/db/db')
+const cors=require('cors')
+connectDB()
 const app=express()
+
+app.use(express.json())
+app.use(cors())
 const PORT=process.env.PORT||5000
 
-if(process.env.MODE==='development')
+if(process.env.NODE_ENV==='development'){
     app.use(morgan('dev'))
+}
+
+const userRoutes=require('./src/routes/userRoutes')
+app.use('/api/users',userRoutes)
+
 
 app.listen(PORT,()=>{console.log(`Server running on the port ${PORT}`.yellow)})
