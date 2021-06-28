aebe35542548f2ffb34c779726ba85b8dd11da62
LAPTOP-SLFMTQK4\abhis <thakurabhinav17122001@gmail.com>
2021-06-28 17:17:06 +0530

    older route

--- src/routes/folderRoutes.js
+++ src/routes/folderRoutes.js
@@ -1,8 +1,9 @@
 const { protect } = require('../middleware/authMiddleware')
-const {createFolder,copyFolder,moveFolder,deleteFolder,getParentFolder}=require('../controllers/folderControllers')
+const {createFolder,copyFolder,moveFolder,deleteFolder,getParentFolder, getFolder}=require('../controllers/folderControllers')
 const router=require('express').Router()
 
 router.route('/:id').get(protect,getParentFolder)
+router.route('/details/:id').get(protect,getFolder)
 router.route('/create').post(protect,createFolder)
 router.route('/move/:id').post(protect,moveFolder)
 router.route('/copy/:id').put(protect,copyFolder)
