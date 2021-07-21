c883cbe5525a963ab777aec0ef128b02c9267ff6
LAPTOP-SLFMTQK4\abhis <thakurabhinav17122001@gmail.com>
2021-07-21 18:03:54 +0530

    Api added

--- src/routes/fileRoutes.js
+++ src/routes/fileRoutes.js
@@ -9,10 +9,10 @@ const {
 	removeFromRecycle,
 } = require("../controllers/fileControllers");
 
-router.route("/create").post(protect, createFile);
-router.route("/move/:id").post(protect, moveFile);
-router.route("/copy/:id").put(protect, copyFile);
-router.route("/delete/:id").delete(protect, deleteFile);
+router.route("/create").post(protect, createFile); // api/files/create
+router.route("/move/:id").post(protect, moveFile); // api/files/move/:id
+router.route("/copy/:id").put(protect, copyFile); // api/files/copy/:id
+router.route("/delete/:id").delete(protect, deleteFile); // api/files/delete/:id
 // router.route('/recycled/:id').put(protect,recycled)
 // router.route('/recover/:id').put(protect,removeFromRecycle)
 module.exports = router;
