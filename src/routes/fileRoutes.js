24ae6cd97d56e7e7116c9cbb8ee8aab7fc6f8b5e
LAPTOP-SLFMTQK4\abhis <thakurabhinav17122001@gmail.com>
2021-07-21 18:01:17 +0530

    :

--- src/routes/fileRoutes.js
+++ src/routes/fileRoutes.js
@@ -1,11 +1,18 @@
-const router=require('express').Router()
-const { protect } = require('../middleware/authMiddleware')
-const {createFile,copyFile,moveFile,deleteFile, recycled, removeFromRecycle}=require('../controllers/fileControllers')
+const router = require("express").Router();
+const { protect } = require("../middleware/authMiddleware");
+const {
+	createFile,
+	copyFile,
+	moveFile,
+	deleteFile,
+	recycled,
+	removeFromRecycle,
+} = require("../controllers/fileControllers");
 
-router.route('/create').post(protect,createFile)
-router.route('/move/:id').post(protect,moveFile)
-router.route('/copy/:id').put(protect,copyFile)
-router.route('/delete/:id').delete(protect,deleteFile)
-router.route('/recycled/:id').put(protect,recycled)
-router.route('/recover/:id').put(protect,removeFromRecycle)
-module.exports=router
+router.route("/create").post(protect, createFile);
+router.route("/move/:id").post(protect, moveFile);
+router.route("/copy/:id").put(protect, copyFile);
+router.route("/delete/:id").delete(protect, deleteFile);
+// router.route('/recycled/:id').put(protect,recycled)
+// router.route('/recover/:id').put(protect,removeFromRecycle)
+module.exports = router;
