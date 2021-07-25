db05c7b3c21b1853eba0e98c720bd715c4fb5a74
LAPTOP-SLFMTQK4\abhis <thakurabhinav17122001@gmail.com>
2021-07-25 12:17:58 +0530

    recycle file

--- src/routes/fileRoutes.js
+++ src/routes/fileRoutes.js
@@ -13,6 +13,6 @@ router.route("/create").post(protect, createFile); // api/files/create
 router.route("/move/:id").post(protect, moveFile); // api/files/move/:id
 router.route("/copy/:id").put(protect, copyFile); // api/files/copy/:id
 router.route("/delete/:id").delete(protect, deleteFile); // api/files/delete/:id
-// router.route('/recycled/:id').put(protect,recycled)
+router.route("/recycled/:id").put(protect, recycled);
 // router.route('/recover/:id').put(protect,removeFromRecycle)
 module.exports = router;
