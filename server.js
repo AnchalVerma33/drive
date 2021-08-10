baf88f9218dc98b99ceed08a23259c01e8cf978f
Aman Dixit <56161073+AMAN123956@users.noreply.github.com>
2021-08-10 11:14:43 +0530

    Added search functionality

--- server.js
+++ server.js
@@ -34,13 +34,16 @@ app.get('/removeAll',async(req,res)=>{
 
 const userRoutes=require('./src/routes/userRoutes')
 const fileRoutes=require('./src/routes/fileRoutes')
-const folderRoutes=require('./src/routes/folderRoutes')
+const folderRoutes = require('./src/routes/folderRoutes')
+const searchRoutes = require('./src/routes/searchRoutes')
+
 const User = require('./src/models/userModel')
 const Folder = require('./src/models/folderModel')
 const File = require('./src/models/fileModel')
 
 app.use('/api/users',userRoutes)
 app.use('/api/files',fileRoutes)
-app.use('/api/folders',folderRoutes)
+app.use('/api/folders', folderRoutes)
+app.use('/api/search', searchRoutes)
 
 app.listen(PORT,()=>{console.log(`Server running on the port ${PORT}`.yellow)})
