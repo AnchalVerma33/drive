1e1433200cbe7aa8c6784470880024936e28249a
LAPTOP-SLFMTQK4\abhis <thakurabhinav17122001@gmail.com>
2021-08-10 11:24:25 +0530

    :

--- src/controllers/searchController.js
+++ src/controllers/searchController.js
@@ -1,29 +1,35 @@
-const File = require('../models/fileModel');
-const Folder = require('../models/folderModel');
-const User = require('../models/userModel');
+const File = require("../models/fileModel");
+const Folder = require("../models/folderModel");
+const User = require("../models/userModel");
 
-const searchByLetter = async(req, res) => {
-    try {
-        const queryString = req.params.query;
-        console.log(queryString);
-        const fileData = await File.find();
-        let data = [];
-        let flag = -1;
-        fileData.map(item => {
-            if (queryString == item.name.substr(0, queryString.length)) {
-                data.push(item);
-            }
-            
-        })
-        console.log("data", data);
-        res.status(201).json({
-				success: true,
-				message: "Successfully Request Made",
-				data: data,
+const searchByLetter = async (req, res) => {
+	try {
+		const queryString = req.params.query;
+		console.log(queryString);
+		const user = req.user;
+		if (!user) {
+			return res.status(401).send({
+				success: false,
+				error: "Unauthorized",
 			});
-    }
-    catch (e) {
-        if (e.name === "ValidationError") {
+		}
+		const userID = user._id;
+
+		const fileData = await File.find({ user: userID });
+		let data = [];
+		fileData.map((item) => {
+			if (queryString == item.name.substr(0, queryString.length)) {
+				data.push(item);
+			}
+		});
+		console.log("data", data);
+		res.status(201).json({
+			success: true,
+			message: "Successfully Request Made",
+			data: data,
+		});
+	} catch (e) {
+		if (e.name === "ValidationError") {
 			console.log(e);
 			const messages = Object.values(e.errors).map((val) => val.message);
 			res.status(400).json({
@@ -37,8 +43,7 @@ const searchByLetter = async(req, res) => {
 				error: `${e}`,
 			});
 		}
-    }
-}
-
+	}
+};
 
 module.exports = { searchByLetter };
